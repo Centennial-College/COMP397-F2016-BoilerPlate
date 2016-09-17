ba0639306a6ba9a7b835bd8ccfc54ffe3db508f0
Kevin Ma <kbma.kevin@gmail.com>
2016-09-17 17:32:02 -0400

    added type definitions for jquery and bootstrap

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,6 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
 /// <reference path="globals/easeljs/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/preloadjs/index.d.ts" />
 /// <reference path="globals/soundjs/index.d.ts" />
 /// <reference path="globals/tweenjs/index.d.ts" />
